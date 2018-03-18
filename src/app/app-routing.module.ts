6835cebe99696cf962c56351fed8a4eae0c969e2
frontend-magic <gabi19781111@gmail.com>
2018-03-18 16:08:48 +0100

    refactor mappaákba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 import {EventComponent} from './event/event.component';
 import {TicketComponent} from './ticket/ticket.component';
 import {AboutComponent} from './about/about.component';
-import {LoginComponent} from './login/login.component';
-import {RegistrationComponent} from './registration/registration.component';
-import {EventListComponent} from './event-list/event-list.component';
-import {EventDetailComponent} from './event-detail/event-detail.component';
-import {ProfileComponent} from './profile/profile.component';
-import {ProfileEditComponent} from './profile-edit/profile-edit.component';
-import {TicketListComponent} from './ticket-list/ticket-list.component';
-import {TicketDetailComponent} from './ticket-detail/ticket-detail.component';
+import {LoginComponent} from './user/login/login.component';
+import {RegistrationComponent} from './user/registration/registration.component';
+import {EventListComponent} from './event/event-list/event-list.component';
+import {EventDetailComponent} from './event/event-detail/event-detail.component';
+import {ProfileComponent} from './user/profile/profile.component';
+import {ProfileEditComponent} from './user/profile-edit/profile-edit.component';
+import {TicketListComponent} from './ticket/ticket-list/ticket-list.component';
+import {TicketDetailComponent} from './ticket/ticket-detail/ticket-detail.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -27,7 +27,9 @@ const routes: Routes = [
   { path: 'user',
     children: [
       {path: '', component: ProfileComponent},
-      {path: 'edit', component: ProfileEditComponent}
+      {path: 'edit', component: ProfileEditComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'registration', component: RegistrationComponent},
     ]
   },
   {
@@ -40,8 +42,6 @@ const routes: Routes = [
     ]
   },
   {path: 'about', component: AboutComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
